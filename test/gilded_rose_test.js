d0c54026c2551cc2b1e481ceef9a254194cce21c
kwasniew <kwasniewski.mateusz@gmail.com>
2020-01-29 11:53:08 +0100

    characterization test

---
+++ test/gilded_rose_test.js
@@ -0,0 +1,17 @@
+const test = require("tape");
+const Item = require("../src/item");
+const Shop = require("../src/shop");
+
+test("normal item before sell date", function(t) {
+    // given
+    const item = new Item('+5 Dexterity Vest', 10, 20);
+    const shop = new Shop([item]);
+
+    // when
+    shop.updateQuality();
+
+    // then
+    t.equal(item.sellIn, 9);
+    t.equal(item.quality, 19);
+    t.end();
+});
